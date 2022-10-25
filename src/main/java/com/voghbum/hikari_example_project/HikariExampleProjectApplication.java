2cab757ab04a7a399b7df9a70f0bdfb97dd7d674
Hüseyin Aşcı <huseyinascci@gmail.com>
2022-10-25 21:14:51 +0300

    initializing commit
    spring web, spring data jpa, h2 database dependencies added.

---
+++ src/main/java/com/voghbum/hikari_example_project/HikariExampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.voghbum.hikari_example_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikariExampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikariExampleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voghbum/hikari_example_project/HikariExampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voghbum.hikari_example_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikariExampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
